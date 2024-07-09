d6cf71c52b62909187d7e7ccffe4474575dadb99
plfor30y <plfor30y@naver.com>
2024-07-09 11:32:27 +0900

    주문을 융통성하게 하기 위해 selenium 기능을 분할함

--- sub/history/purchase_history.py
+++ sub/history/purchase_history.py
@@ -1,9 +1,25 @@
 import json
 from datetime import datetime
 import os
+# from sub.buy import item
 
 
-def record_purchase_history(products_purchased: list):
+class item:
+    name = ''
+    price = ''
+    arrival_info = ''
+    link = ''
+    quantity = 0
+
+    def __init__(self, name, price='', arrival_info='', link='', quantity=0):
+        self.name = name
+        self.price = price
+        self.arrival_info = arrival_info
+        self.link = link
+        self.quantity = quantity
+
+
+def record_purchase_history(item_purchased: item):
     file_path = './sub/history/purchase_history.json'
     if os.path.exists(file_path):
         try:
@@ -11,19 +27,19 @@ def record_purchase_history(products_purchased: list):
             with open(file_path, 'r', encoding='utf-8') as file:
                 history = json.load(file)
 
-            if len(history) > 50:
-                # history 비우기
-                history = {}
-
-            # {현재 시간 : 주문된 상품들 정보} 업데이트
-            history.update({
-                now_time_stamp(): products_purchased
-            })
+            if now_time_stamp() in history:
+                history[now_time_stamp()].append({
+                    'name': item_purchased.name, 'link': item_purchased.link, 'quantity': item_purchased.quantity
+                })
+            else:
+                history[now_time_stamp()] = []
+                history[now_time_stamp()].append({
+                    'name': item_purchased.name, 'link': item_purchased.link, 'quantity': item_purchased.quantity
+                })
 
             # 다시 써주기
             with open(file_path, 'w', encoding='utf-8') as file:
                 json.dump(history, file, ensure_ascii=False, indent=4)
-
         except Exception as e:
             print(e)
 
@@ -32,7 +48,13 @@ def record_purchase_history(products_purchased: list):
 
             # {현재 시간 : 주문된 상품들 정보} 저장
             history = {
-                now_time_stamp(): products_purchased
+                now_time_stamp(): [
+                    {
+                        'name': item_purchased.name,
+                        'link': item_purchased.link,
+                        'quantity': item_purchased.quantity
+                    }
+                ]
             }
 
             # 파일에 써주기
@@ -45,4 +67,17 @@ def record_purchase_history(products_purchased: list):
 
 def now_time_stamp() -> str:
     now = datetime.now()
-    return f'{now.month}/{now.day}/{now.hour}/{now.minute}'
+    return f'{now.month}/{now.day}'
+
+
+test_item1 = item(name='[새로운상회] 새 제품1', link='https://www.example.com', quantity=1)
+test_item2 = item(name='[새로운상회] 새 제품2', link='https://www.example.com', quantity=2)
+test_item3 = item(name='[새로운상회] 새 제품3', link='https://www.example.com', quantity=3)
+test_item4 = item(name='[새로운상회] 새 제품4', link='https://www.example.com', quantity=4)
+test_item5 = item(name='[새로운상회] 새 제품5', link='https://www.example.com', quantity=5)
+
+record_purchase_history(test_item1)
+record_purchase_history(test_item2)
+record_purchase_history(test_item3)
+record_purchase_history(test_item4)
+record_purchase_history(test_item5)
--- sub/work_flow.py
+++ sub/work_flow.py
@@ -1,5 +1,5 @@
 import sub.make_voice as mv
-import sub.extractor as ex
+import sub.extractor_test as ex
 import sub.buy as buy
 import os
 from STT import listen_to_client as ltc
@@ -16,19 +16,23 @@ sound_path = './sound/'
 
 class controller:
 
+    selen = None
+
     def __init__(self):
         self.stage = 'call_ai'
-        self.order = ''
-        self.order_items = []
-        self.response_about_order_confirm = False
+        self.order = ''  # 사람 말을 여기다 저장
+        self.order_search_word = []  # 주문 검색 목록 저장 (보통 카테고리가 저장됨 eg: {item:딸기, quantity:10})
+        self.order_item = []  # item class 인스턴스가 들어감
+        self.add_order_items = []  # 추가 주문 목록 저장
 
     def recognize_voice_interface(self):
         go_to_stage = {
-            'call_ai': self.ai호출,          # ai 호출
-            'order': self.주문받기,          # 주문
-            'order_confirm': self.주문확인,  # 주문확인
-            'go_to_mall': self.장바구니담기,
-            'order_add': self.주문추가       # 주문추가
+            'call_ai': self.ai호출,
+            'order_listen': self.주문듣기,
+            'order_confirm': self.주문확인,
+            'order_search': self.주문검색,
+            'order_add': self.주문추가,
+            'end': self.끝
         }
         # stage에 맞는 함수 반환
         return go_to_stage[self.stage]
@@ -39,14 +43,13 @@ class controller:
         if ai_name in call_ai:
             chat.cm.write_chat(chat.chat('client', ai_name))
             # order stage로 이동
-            self.stage = 'order'
+            self.stage = 'order_listen'
 
-    def 주문받기(self):
+    def 주문듣기(self):
 
         # 사용자에게 주문받는 다는 신호를 주고
         chat.cm.write_chat(chat.chat('ai', '주문받는중'))
         mv.playsound(sound_path + 'water_drop.mp3')
-        print('주문받는중')
 
         # 주문듣기 10초
         self.order = lc.listen(10)
@@ -60,53 +63,104 @@ class controller:
 
     def 주문확인(self):
 
-        self.order_items = ex.ProductQuantityExtractor().extract(self.order)
-        print(self.order_items)
-        if len(self.order_items) == 0:
+        # 말에서 주문 추출
+        self.order_search_word = ex.ProductQuantityExtractor().extract(self.order)
+        # print(self.order_items)
+        if len(self.order_search_word) == 0:
             chat.cm.write_chat(chat.chat('ai', '주문하시려면 다시 저를 불러주세요'))
-            mv.playsound(sound_path + 'call_me_again.mp3')  # 주문확인중
+            mv.playsound(sound_path + 'call_me_again.mp3')  # 주문하시려면 다시 저를 불러주세요
             self.stage = 'call_ai'  # 다시 ai 호출을 기다리도록 함
             return
 
         # 주문한 상품이 정확한지 확인하기
         voice = '주문하신 상품이\n'
-        for product in self.order_items:
+        for product in self.order_search_word:
             voice += f'{product["item"]}, {product["quantity"]}개\n'
         voice += '맞으십니까?'
-
         chat.cm.write_chat(chat.chat('ai', voice))
         mv.make_voice(voice)  # 확인하는 음성출력
+        mv.playsound(sound_path + 'water_drop.mp3')
 
-        res = lc.listen(2)  # 주문에 대한 확인 음성 받기
+        # 주문 확인 받기
+        res = lc.listen(4)  # 주문에 대한 확인 음성 받기
         chat.cm.write_chat(chat.chat('client', res))
-        for answer_yes in ['응', '그렇게 해', '어', '그래', '구매해', '주문해', '구매', '주문']:
+
+        # 긍정응답이 존재하는지 확인
+        for answer_yes in ['응', '그렇게 해', '어', '그래', '구매해', '주문해', '구매', '주문', '맞아', '네']:
             if '안해' not in res and '취소' not in res and answer_yes in res:
                 chat.cm.write_chat(chat.chat('ai', "주문중"))
                 mv.playsound(sound_path + 'ordering.mp3')  # 주문중 안내
-                self.stage = 'go_to_mall'  # 장바구니에 담으러 이동
+                self.stage = 'order_search'  # 주문검색
                 return
 
-        # 긍정응답이 없을 경우
+        # 주문실패
         chat.cm.write_chat(chat.chat('ai', '다시 주문해주세요'))
         mv.playsound(sound_path + 'get_back_order.mp3')  # 다시 주문해주세요
-        self.stage = "order"  # 다시 주문으로 이동
+        self.stage = "order_listen"  # 다시 주문으로 이동
 
-    def 장바구니담기(self):
+    # 주문을 검색 + 사용자에게 어떤 상품을 고르겠냐고 물어봄
+    def 주문검색(self):
 
-        id = os.getenv('COUPANG_ID')
-        pw = os.getenv('COUPANG_PW')
         try:
+            self.selen = buy.buyBySelenium()
+            self.selen.go_to_mall()  # 홈페이지 이동
+            self.selen.login()  # 홈페이지 로그인
+            for search_word in self.order_search_word:
+                print(search_word)
+                voice = f'{search_word["item"]}를 검색합니다.'
+                chat.cm.write_chat(chat.chat('ai', voice))
+                mv.make_voice(voice)  # 음성만들고 출력
 
-            go_to_mall = buy.buyBySelenium()
-            go_to_mall.getUserInfo(id, pw)
-            go_to_mall.login()
-            go_to_mall.searchItem(self.order_items)
-            go_to_mall.cart()
+                self.selen.search_item(search_word['item'])  # 검색
+                items = self.selen.get_related_items()  # 검색된 창에서 상품 상위 4개 추출
+                voice = '상위 4개의 상품 중에\n'
+                i = 1
+                for item in items:
+                    voice += f'{i}번. {item.name} {item.price}원, {item.arrival_info}\n'
+                    i += 1
 
+                voice += '들이 있어요. 어떤 걸 고르시겠어요? 1 2 3 4 로 말해주세요'
+                mv.make_voice(voice)  # 음성만들고 출력
+                res = lc.listen(3)  # 주문에 대한 확인 음성 받기
+                chat.cm.write_chat(chat.chat('client', res))
+                # selected = items[int(res)-1]
+                selected = items[0]  # 테스트용
+                selected.quantity = int(search_word['quantity'])  # 상품 개수 넣어주고
+                self.selen.go_to_add_cart(selected)     # 장바구니에 담으러 감
+                ph.record_purchase_history(selected)  # 구매이력 담기
+                self.stage = "order_add"
+        except Exception as e:
+            print(e)
+            mv.playsound(sound_path+'something_wrong.mp3')
+            self.stage = "call_ai"  # 다시 ai 호출을 기다리도록 함
+
+    def 주문추가(self):
+        # 추가하실 것이 더 있습니까?
+        chat.cm.write_chat(chat.chat('ai', "추가하실 것이 더 있으신가요?"))
+        mv.playsound(sound_path + 'is_there_something_to_add.mp3')
+        mv.playsound(sound_path + 'water_drop.mp3')
+
+        # 주문 확인 받기
+        res = lc.listen(2)  # 주문에 대한 확인 음성 받기
+        chat.cm.write_chat(chat.chat('client', res))
+
+        for answer_yes in ['응', '그렇게 해', '어', '그래', '맞아', '네']:
+            if '안해' not in res and '취소' not in res and answer_yes in res:
+                # 추가주문해주세요
+                chat.cm.write_chat(chat.chat('ai', "추가 주문해주세요"))
+                mv.playsound(sound_path + 'add_items_in_cart.mp3')
+                self.stage = 'order_listen'  # 주문듣기로 이동
+                return
+
+        # 장바구니로 이동합니다.
+        self.stage = 'end'
+
+    def 끝(self):
+
+        try:
             # 요청하신 상품을 장바구니에 모두 담았습니다.
             chat.cm.write_chat(chat.chat('ai', "요청하신 상품을 장바구니에 모두 담았습니다"))
             mv.playsound(sound_path + 'request_item_all_clear.mp3')
-            ph.record_purchase_history(self.order_items)
 
         except Exception as e:
             print(e)
@@ -114,6 +168,3 @@ class controller:
 
         finally:
             self.stage = "call_ai"  # 다시 ai 호출을 기다리도록 함
-
-    def 주문추가():
-        pass
